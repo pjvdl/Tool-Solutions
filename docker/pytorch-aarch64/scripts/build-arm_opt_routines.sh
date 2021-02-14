ee8e71c971496fecd895eeedc9db0e612d407087
Paul van der Linden <paul@outthought.co>
2021-02-15 09:15:59 +1030

    fixes to arm opt routines

--- docker/pytorch-aarch64/scripts/build-arm_opt_routines.sh
+++ docker/pytorch-aarch64/scripts/build-arm_opt_routines.sh
@@ -24,16 +24,13 @@ readonly package=arm_opt_routines
 # Pick a specific commit to make sure the confic we use is safe
 # Note: we use a custom config to force use of GCC7 to avoid a
 # bug with GCC9's default binutils
-readonly version=433a3b1ff9b60f4baf7c30f2e1908b9629968a41
+readonly version=v20.11
 readonly src_host=https://github.com/ARM-software
 readonly src_repo=optimized-routines
 
 git clone ${src_host}/${src_repo}.git
 cd ${src_repo}
-git checkout $version -b feature/gcc-7
-
-export CFLAGS="-mcpu=native -O3"
-export LDFLAGS="-lpthread"
+git checkout $version
 
 install_dir=$PROD_DIR/$package 
 cp ../config.mk config.mk
